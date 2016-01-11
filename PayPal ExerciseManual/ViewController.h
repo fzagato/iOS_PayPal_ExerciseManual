ea7bf2b122deebc4dc7d4380e2ffaf5a396a5b1e
fzagato <fzagato@gmail.com>
2016-01-11 15:53:55 -0600

    Initial Commit

---
+++ PayPal ExerciseManual/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PayPal ExerciseManual
+//
+//  Created by Francisco Javier Trujillo Lopez on 11/01/16.
+//  Copyright © 2016 FZagato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PayPal ExerciseManual/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PayPal ExerciseManual
+//
+//  Created by Francisco Javier Trujillo Lopez on 11/01/16.
+//  Copyright © 2016 FZagato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
